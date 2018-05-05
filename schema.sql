5d44eb8d0b275f0b3880663a0ff500e00cb3c33f
Matthew Paulino <matthewpaulino@Matthews-MacBook-Pro.local>
2018-05-05 14:36:21 -0700

    added sql file containing ititial database and tables

--- schema.sql
+++ schema.sql
@@ -1,4 +1,25 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS todolist;
--- Creates the "todolist" database --
-CREATE DATABASE todolist;
+CREATE DATABASE screenmatch_db;
+USE screenmatch_db;
+
+
+
+CREATE TABLE user(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  firstName VARCHAR(100) NOT NULL,
+  lastName VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL, 
+  password VARCHAR(100) NOT NULL,
+  action VARCHAR(100) NOT NULL,
+  q1_fovorite_actor VARCHAR(100) NOT NULL, 
+  q2_favorite_director VARCHAR(100) NOT NULL,
+  q3_ratings VARCHAR(100) NOT NULL,
+  platforms VARCHAR(100) NOT NULL,
+  genre VARCHAR(100) NOT NULL, 
+  
+  CREATE TABLE list(
+  listingId INTEGER(11) AUTO_INCREMENT NOT NULL,
+  userId INTEGER(11) NOT NULL,
+  is_favorite BOOLEAN,
+  watch_list_rank INTEGER(11) NOT NULL,
+  PRIMARY KEY (id)
+);
