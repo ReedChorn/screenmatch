e346f20c7bf4a0c9b76e49fa26a2a1b4c1cd74d6
Shayan Shakiba <shayanshakiba@dhcp-v124-018.mobile.uci.edu>
2018-05-08 18:54:21 -0700

    fixed nodemon server error by commenting-out undefined functions

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -22,17 +22,17 @@ module.exports = function(app) {
   })
 
   // POST route for saving a new todo. You can add a film to favorites using the data on req.body
-  app.post("/api/favorites", function(req, res) {
+  // app.post("/api/favorites", function(req, res) {
 
-  });
+  // });
 
-  // DELETE route for deleting favorite movies. You can access the favorites id in req.params.id
-  app.delete("/api/favorites/:id", function(req, res) {
+  // // DELETE route for deleting favorite movies. You can access the favorites id in req.params.id
+  // app.delete("/api/favorites/:id", function(req, res) {
 
-  });
+  // });
 
-  // PUT route for updating user movie preferences. The updated preferences will be available in req.body
-  app.put("/api/preferences", function(req, res) {
+  // // PUT route for updating user movie preferences. The updated preferences will be available in req.body
+  // app.put("/api/preferences", function(req, res) {
 
-  });
+  // });
 };
